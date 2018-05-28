ef2131d738e3b0f2265d7af31894749d841e975f
manvinitb <manvi.nitb@gmail.com>
2018-05-29 00:32:59 +0530

    logging

--- orders/src/main/java/com/shopstreet/backend/orders/OrdersApplication.java
+++ orders/src/main/java/com/shopstreet/backend/orders/OrdersApplication.java
@@ -1,12 +1,15 @@
 package com.shopstreet.backend.orders;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class OrdersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersApplication.class, args);
+		log.info("Testing logging!!!!");
 	}
 }
